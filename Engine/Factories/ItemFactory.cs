240be3463eaceac30bcd97aa333fdb8d649daa65
Ifly2sky <tinhau24@gmail.com>
2023-02-13 10:42:10 +0200

    created Monster and MonsterFactory classes. added images for monsters

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -1,10 +1,6 @@
 ﻿using Engine.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Engine.Factories
 {
@@ -18,8 +14,12 @@ namespace Engine.Factories
                 new Weapon(1001, "Pointy Stick", 1, 1, 2),
                 new Weapon(1002, "Rusty Sword", 5, 1, 3),
                 new GameItem(9001, "Snake fang", 1),
-                new GameItem(9002, "Snakeskin", 2)
-        };
+                new GameItem(9002, "Snakeskin", 2),
+                new GameItem(9003, "Rat tail", 1),
+                new GameItem(9004, "Rat fur", 2),
+                new GameItem(9005, "Spider fang", 1),
+                new GameItem(9006, "Spider silk", 2)
+            };
         }
         public static GameItem CreateGameItem(int itemTypeId)
         {
