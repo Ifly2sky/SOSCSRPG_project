2b92828ee57c503209f58d81e4d620919ca702a4
Ifly2sky <tinhau24@gmail.com>
2023-02-14 09:25:37 +0200

    cleaned up some code

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -6,7 +6,7 @@ namespace Engine.Factories
 {
     public static class ItemFactory
     {
-        private static List<GameItem> _standardGameItems;
+        private static readonly List<GameItem> _standardGameItems;
         static ItemFactory()
         {
             _standardGameItems = new List<GameItem>
