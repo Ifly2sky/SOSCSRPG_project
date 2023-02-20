b246489362709dd3b1f96ab83b4937916662a3ef
Ifly2sky <tinhau24@gmail.com>
2023-02-20 09:53:21 +0200

    added grouped items

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -11,12 +11,14 @@ namespace Engine.Models
         public int ItemTypeID { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
+        public bool IsUnique { get; set; }
 
-        public GameItem(int itemTypeId, string name, int price)
+        public GameItem(int itemTypeId, string name, int price, bool isUnique = false)
         {
             ItemTypeID = itemTypeId;
             Name = name;
             Price = price;
+            IsUnique = isUnique;
         }
 
         public GameItem Clone()
