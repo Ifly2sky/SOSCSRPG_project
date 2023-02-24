5ad80b72a9f7f0b5293ea524f1ed4a2db9b77978
Ifly2sky <tinhau24@gmail.com>
2023-02-24 13:17:31 +0200

    moved traders, recipes and quests to external files

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -77,7 +77,7 @@ namespace Engine.Factories
                 return;
             }
             location.TraderHere =
-                TraderFactory.GetTraderByName(traderHere.AttributeAsString("Name"));
+                TraderFactory.GetTraderByID(traderHere.AttributeAsInt("ID"));
         }
     }
 }
