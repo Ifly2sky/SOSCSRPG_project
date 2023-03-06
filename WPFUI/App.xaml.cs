32a25cae9b36fd3bb3a10a7da7179e16a38d124b
Ifly2sky <tinhau24@gmail.com>
2023-03-06 09:34:34 +0200

    added catching and logging exceptions

--- WPFUI/App.xaml.cs
+++ WPFUI/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Engine.Services;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WPFUI
 {
@@ -13,5 +15,11 @@ namespace WPFUI
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText = $"An exception occured: {e.Exception.Message}\r\n\r\nat: {e.Exception.StackTrace}";
+            LoggingService.Log(e.Exception);
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception", MessageBoxButton.OK);
+        }
     }
 }
