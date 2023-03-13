ad995c919c6051abb465df5c16724b036e3d0c75
Ifly2sky <tinhau24@gmail.com>
2023-03-13 09:31:49 +0200

    added saving and loading

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -14,7 +15,7 @@ namespace WPFUI
     public partial class MainWindow : Window
     {
         private readonly MessageBroker _messageBroker = MessageBroker.GetInstance();
-        private readonly GameSession _gameSession = new GameSession();
+        private readonly GameSession _gameSession;
         private readonly Dictionary<Key, Action> _userInputActions = new Dictionary<Key, Action>();
         public MainWindow()
         {
@@ -22,6 +23,7 @@ namespace WPFUI
             InitializeUserInputActions();
 
             _messageBroker.OnMessageRaised += OnGameMessageRaised;
+            _gameSession = SaveGameService.LoadLastSaveOrCreateNew();
             DataContext = _gameSession;
         }
         private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
@@ -98,6 +100,10 @@ namespace WPFUI
                     }
                 }
             }
+        }
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            SaveGameService.Save(_gameSession);
         }
     }
 }
